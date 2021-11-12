ceef51fac8d595fc30193fa0e1c26d6230bd3e1e
Steven Ng <szeyungng@gmail.com>
2021-11-12 01:45:55 -0700

    Included Matlab file/script for curve-fitting
    - Run script in MatLab, then under `tool` use `Basic Fitting`
    - At the moment, Lab was conducting utilizing a 5th degree polynomial
    - NOTE: use at least 4 significant figure for appropriate curve fitting

---
+++ enel453/lab3/Submissions/plottingDataForCurve.m
@@ -0,0 +1,8 @@
+%From Datasheet Characteristics Chart
+distance = [3.5,   4,    5,   6,    7,    8,   9,   10,   12,   14,   16,   18,   20,   25,   30,   35,   40];
+voltage  = [  3, 2.7, 2.35, 2.2, 1.75, 1.57, 1.4, 1.26, 1.07, 0.93, 0.81, 0.72, 0.67, 0.52, 0.43, 0.38, 0.33];
+
+d_10minus4m = distance .* 100;
+voltage_mV = voltage .*1000;
+
+plot(voltage_mV,d_10minus4m);
